57735dddd3ab11d9697070f02ada86d4d5ed07a8
iMcG33k <goleft186@gmail.com>
2017-06-27 13:51:16 +0800

    label copy menuItem

---
+++ JKCategories/UIKit/UILable/UILabel+Copy.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+Copy.h
+//  NTOFFICE_TS
+//
+//  Created by Naton on 2017/6/27.
+//  Copyright © 2017年 natonAir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Copy)
+
+@property (nonatomic,assign) BOOL isCopyable;
+
+@end
